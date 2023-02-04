85bdb832781799dc867ca569aa1decd6950631e0
M Haidar Hanif <git@mhaidarhanif.com>
2023-02-04 12:21:18 +0700

    feat: convert currency function

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const formConvertElement = document.getElementById("form-converter");
+const outputAmountElement = document.getElementById("output-amount");
+const outputResultElement = document.getElementById("output-result");
+
+function convertCurrency(amount) {
+  const exchangeRate = 15_000; // get from actual data/array
+
+  const result = amount * exchangeRate;
+
+  return result;
+}
 
 formConvertElement.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -6,6 +16,8 @@ formConvertElement.addEventListener("submit", (event) => {
   const formData = new FormData(formConvertElement);
 
   const amount = Number(formData.get("amount"));
+  const result = convertCurrency(amount);
 
-  console.log({ amount });
+  outputAmountElement.innerHTML = amount;
+  outputResultElement.innerHTML = result;
 });
