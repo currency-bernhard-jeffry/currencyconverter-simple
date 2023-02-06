629f8761df60b8e3e6a90dd44a698fae3f3db62d
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-02-06 21:49:42 +0800

    create button separately to get API manually only when the button clicked

--- trial1.js
+++ trial1.js
@@ -3,6 +3,7 @@ const inputForm = document.getElementById("input-form");
 const getInputElementId = document.getElementById("input-field");
 const getOutputElementId = document.getElementById("output-field");
 const rateCheck = document.getElementById("check-rate");
+const fetchUrlCheck = document.getElementById("check-fetch-url");
 // const submitButton = document.getElementById("button-submit");
 
 // will be inside a function
@@ -13,3 +14,8 @@ inputForm.addEventListener("submit", (event) => {
   getOutputElementId.innerText = output;
   rateCheck.innerHTML = rate;
 });
+
+// function to get api
+const url = `https://api.exchangerate.host/convert?from=SGD&to=IDR`;
+const response = fetch(url);
+console.log(response);
