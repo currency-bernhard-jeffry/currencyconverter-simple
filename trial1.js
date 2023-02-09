0a4d47ae2e600e9c186c67a237d2ab73ac9f61b2
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-02-09 22:22:52 +0800

    successfully get symbols API and print at Console

--- trial1.js
+++ trial1.js
@@ -13,24 +13,30 @@ const liveRate = document.getElementById("live-rate");
 // will be inside a function
 inputForm.addEventListener("submit", async (event) => {
   event.preventDefault();
-  const urlLive = "https://api.exchangerate.host/convert?from=SGD&to=IDR";
-  const response = await fetch(urlLive);
+  fetch("https://api.exchangerate.host/convert?from=SGD&to=IDR")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  const sgdToIdrConvertUrl =
+    "https://api.exchangerate.host/convert?from=SGD&to=IDR";
+  const response = await fetch(sgdToIdrConvertUrl);
   const data = await response.json();
-  const liveRate = data.result.toFixed(2);
+  const liveRate = data.result;
   // const rate = 11500;
-  const output = Number(getInputElementId.value * liveRate);
+  // const output = Number(getInputElementId.value * liveRate);
+  const output = getInputElementId.value * liveRate;
+  const outputTwoDecimalRoundUp = output.toFixed(2);
   getOutputElementId.innerText =
-    getInputElementId.value + " SGD = " + output + " IDR";
+    getInputElementId.value + " SGD = " + outputTwoDecimalRoundUp + " IDR";
   rateCheck.innerText = `1 SGD = ${liveRate} IDR`;
 });
 
 // test function to get API data (only applied to the optional 'Get API' button)
 buttonGetApi.addEventListener("click", async () => {
-  const url = `https://api.exchangerate.host/convert?from=SGD&to=IDR`;
-  const response = await fetch(url);
+  // const url = `https://api.exchangerate.host/convert?from=SGD&to=IDR`;
+  // const response = await fetch(url);
   // .then((response) => response.json())
   // .then((data) => console.log(data));
-  const data = await response.json();
+  // const data = await response.json();
   // console.log(response);
   // console.log(response.query);
   // console.log(data);
@@ -38,5 +44,13 @@ buttonGetApi.addEventListener("click", async () => {
   // console.log(data.query.from);
   // console.log(data.query.to);
   // console.log(data.result.toFixed(2));
-  liveRate.innerText = `Live Rate = ${data.result.toFixed(2)}`;
+  // liveRate.innerText = `Live Rate = ${data.result.toFixed(2)}`;
+  fetch("https://api.exchangerate.host/symbols")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  const symbolsUrl = "https://api.exchangerate.host/symbols";
+  const response = await fetch(symbolsUrl);
+  const dataSymbols = await response.json();
+  const symbolsCode = dataSymbols.symbols;
+  console.log(symbolsCode);
 });
