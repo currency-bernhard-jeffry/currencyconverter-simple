121436caf094ac77217d47eb7d54da28457576df
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-02-09 23:39:49 +0800

    removed unnecessary lines and comments

--- trial1.js
+++ trial1.js
@@ -1,13 +1,9 @@
-// trial 1
 const inputForm = document.getElementById("input-form");
 const getInputElementId = document.getElementById("input-field");
 const getOutputElementId = document.getElementById("output-field");
 const rateCheck = document.getElementById("check-rate");
 const fetchUrlCheck = document.getElementById("check-fetch-url");
-// const submitButton = document.getElementById("button-submit"); <- not necessary within a form
-
 const buttonGetApi = document.getElementById("button-get-api");
-
 const liveRate = document.getElementById("live-rate");
 
 // will be inside a function
@@ -21,10 +17,8 @@ inputForm.addEventListener("submit", async (event) => {
   const response = await fetch(sgdToIdrConvertUrl);
   const data = await response.json();
   const liveRate = data.result;
-  // const rate = 11500;
-  // const output = Number(getInputElementId.value * liveRate);
   const output = getInputElementId.value * liveRate;
-  const outputTwoDecimalRoundUp = output.toFixed(2);
+  const outputTwoDecimalRoundUp = Number.parseFloat(output).toFixed(2);
   getOutputElementId.innerText =
     getInputElementId.value + " SGD = " + outputTwoDecimalRoundUp + " IDR";
   rateCheck.innerText = `1 SGD = ${liveRate} IDR`;
@@ -32,19 +26,6 @@ inputForm.addEventListener("submit", async (event) => {
 
 // test function to get API data (only applied to the optional 'Get API' button)
 buttonGetApi.addEventListener("click", async () => {
-  // const url = `https://api.exchangerate.host/convert?from=SGD&to=IDR`;
-  // const response = await fetch(url);
-  // .then((response) => response.json())
-  // .then((data) => console.log(data));
-  // const data = await response.json();
-  // console.log(response);
-  // console.log(response.query);
-  // console.log(data);
-  // console.log(data.query);
-  // console.log(data.query.from);
-  // console.log(data.query.to);
-  // console.log(data.result.toFixed(2));
-  // liveRate.innerText = `Live Rate = ${data.result.toFixed(2)}`;
   fetch("https://api.exchangerate.host/symbols")
     .then((response) => response.json())
     .then((data) => console.log(data));
@@ -59,6 +40,4 @@ buttonGetApi.addEventListener("click", async () => {
   console.log(symbolsCode[1]);
   const symbolsCodeParsed = JSON.parse(symbolsCode);
   console.log(symbolsCodeParsed);
-
-  // symbolsCode.forEach((symbol) => console.log(symbolsCode.code));
 });
